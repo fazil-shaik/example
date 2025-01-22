e18f9bc55d8b8787b3e98cd7e0fa3c7cbf5196d7
shaik fazil basha <fazilshaik103@gmail.com>
2025-01-22 10:28:50 +0530

    added changes

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello World');
     }
 );
+
+app.get('/about', (req, res) => {   
+    res.send('About Us');
+})
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
     }  
